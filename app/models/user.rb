771d356c6fc9636de8b7611f78a1dff65f79f929
Mahhek <mahhek@mahhek-laptop.(none)>
2012-05-02 13:42:23 +0500

    unfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 #  belongs_to :country
 
   devise :database_authenticatable, :registerable, :confirmable,
-    :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:username]
+    :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name,
