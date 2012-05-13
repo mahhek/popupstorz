52f15d6903ccde0337fd94334abfd7ca4c62cf62
mahhek <mahhek.khan@gmail.com>
2012-05-14 00:55:30 +0500

    fix

--- db/migrate/20120512162733_add_want_to_bargain_in_offers.rb
+++ db/migrate/20120512162733_add_want_to_bargain_in_offers.rb
@@ -1,6 +1,6 @@
 class AddWantToBargainInOffers < ActiveRecord::Migration
   def up
-    add_column :offers, :want_to_bargain, :boolean, :default => 0
+    add_column :offers, :want_to_bargain, :boolean, :default => false
   end
 
   def down
