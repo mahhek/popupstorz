11061472c6926e546a04833711b849168818c109
nouman <nouman@ubuntu.(none)>
2012-07-30 16:58:44 +0500

    New potential issues removed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -392,10 +392,14 @@ class ItemsController < ApplicationController
   def created_coming_gatherings
     @gatherings = Offer.find(:all, :conditions => ["(owner_id != ? and user_id = ?) and persons_in_gathering is not NULL and parent_id is NULL and rental_start_date >= '#{Date.parse("#{Date.today}","%Y-%d-%m")}'",current_user.id,current_user.id])
     #    @gatherings = Offer.find(:all, :conditions => ["(owner_id != ? and user_id = ?) and persons_in_gathering is not NULL and parent_id is NULL and rental_start_date >= '#{Date.parse("#{Date.today}","%Y-%d-%m")}' and offers.status !='joinings approved'",current_user.id,current_user.id], :order => "rental_start_date ASC")
-    gathers = @gatherings.group_by(&:item_id)
-    gathers.each do|k,v|
-      @gatherings = v
-    end
+    
+#    gathers = @gatherings.group_by(&:item_id)
+#    gathers.each do|k,v|
+#      @gatherings = v
+#    end
+#    p "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",@gatherings.inspect
+#    ddd
+    
     
     @gatherings = @gatherings + current_user.gatherings.where("owner_id != #{current_user.id} and persons_in_gathering is not NULL and parent_id is NULL and rental_start_date >= '#{Date.parse("#{Date.today}","%Y-%d-%m")}' and offers.status != 'Applied' and offers.status != 'all joinings approved' and offers.status !='joinings approved'", :order => "rental_start_date ASC")
     @gatherings = @gatherings.uniq
