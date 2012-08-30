4a3d1c7c173c43f35d8802e5b4a2604e83c7cb13
Zubair <zubair@zubair-Inspiron-N4050.(none)>
2012-08-30 18:51:13 +0500

    user profile image sizes

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -9,7 +9,7 @@ class Avatar < ActiveRecord::Base
     :s3_credentials => {:access_key_id => "AKIAIETPPQ55S3QZGFQA" ,:secret_access_key => "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"},
     :bucket => "rentareto",
     :path => "/system/ugc/:class/:id/:style/:basename.:extension",
-    :styles => {:thumb => "125x125>", :small => "150x150>", :medium => "275x275>", :large => "400x400>" },
+    :styles => {:thumb => "125x125>", :small => "96X72", :medium => "240x180>", :large => "480x360>" },
     :default_style => :original
 
 
