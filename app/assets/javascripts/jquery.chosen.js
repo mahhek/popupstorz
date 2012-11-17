c45a85372c087dbc9a4d00c592bd5c3732677675
nouman <nouman@ubuntu.(none)>
2012-11-17 21:39:25 +0500

    chosen added

--- app/assets/javascripts/jquery.chosen.js
+++ app/assets/javascripts/jquery.chosen.js
@@ -1,4 +1,5 @@
-// Chosen, a Select Box Enhancer for jQuery and Protoype
+
+// // Chosen, a Select Box Enhancer for jQuery and Protoype
 // by Patrick Filler for Harvest, http://getharvest.com
 // 
 // Version 0.9.8
