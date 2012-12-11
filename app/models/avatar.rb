af8bc8e0ecfeb52fe5adc5a1364dcf6e57703099
nouman <nouman@ubuntu.(none)>
2012-12-11 22:50:58 +0500

    Home page fixes

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -8,7 +8,7 @@ class Avatar < ActiveRecord::Base
     :s3_credentials => {:access_key_id => "AKIAIETPPQ55S3QZGFQA" ,:secret_access_key => "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"},
     :bucket => "rentareto",
     :path => "/system/ugc/:class/:id/:style/:basename.:extension",
-    :styles => {:thumb => "125x125>", :small => "96X72", :medium => "240x180>", :large => "480x360>" },
+    :styles => {:thumb => "125x125>", :small => "96X72", :medium => "240x180>", :large => "480x360>",:home => "266X177" },
     :default_style => :original
 end
 
